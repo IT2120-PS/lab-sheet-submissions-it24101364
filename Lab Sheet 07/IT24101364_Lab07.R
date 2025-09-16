c31b437aa248f0a241bb10f6f5adc7e03b3ae42a
it24101364 <it24101364@my.sliit.lk>
2025-09-17 00:03:09 +0530

    IT24101364_lab_07

---
+++ Lab Sheet 07/IT24101364_Lab07.R
@@ -0,0 +1,11 @@
+#1)
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+#2)
+pexp(2, rate = 1/3)
+
+#3)i)
+pnorm(130, mean = 100, sd = 15, lower.tail = FALSE)
+
+#3)ii)
+qnorm(0.95, mean = 100, sd = 15)
